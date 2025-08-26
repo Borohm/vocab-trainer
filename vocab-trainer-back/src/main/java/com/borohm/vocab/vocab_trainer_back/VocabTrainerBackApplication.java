e03a7c650d3f0ae5940a078ffa52b6206317db3d
Anthony MICHALLET <amichallet@consultants-solutec.fr>
2025-08-26 15:09:54 +0200

    Premier commit

---
+++ vocab-trainer-back/src/main/java/com/borohm/vocab/vocab_trainer_back/VocabTrainerBackApplication.java
@@ -0,0 +1,13 @@
+package com.borohm.vocab.vocab_trainer_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocabTrainerBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocabTrainerBackApplication.class, args);
+	}
+
+}
